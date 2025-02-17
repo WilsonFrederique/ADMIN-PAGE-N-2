8c0c7d2124c05d1d0dcc94355e9038efb12d2bbf
WilsonFrederique <wilsonfrederique3@gmail.com>
2025-02-17 21:21:33 +0300

    MODE SOMBRE

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -45,7 +45,7 @@ const Login = () => {
                         <form>
                             <div className={`form-group position-relative ${inputIndex===0 && 'focus'}`}>
                                 <span className="icon"><MdEmail /></span>
-                                <input type="text" className='form-control' placeholder='Enter your email' onFocus={()=>focusInput(0)} onBlur={()=>setInputIndex(null)} />
+                                <input autoFocus type="text" className='form-control' placeholder='Enter your email' onFocus={()=>focusInput(0)} onBlur={()=>setInputIndex(null)} />
                             </div>
 
                             <div className={`form-group position-relative ${inputIndex===1 && 'focus'}`}>
